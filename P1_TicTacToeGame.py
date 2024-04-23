552aff011a76076e5fd0be9830c27b1ba18fc2ca
SG-ANSH <137523931+SG-ANSH@users.noreply.github.com>
2024-04-23 21:58:51 +0530

    Update P1_TicTacToeGame.py

--- P1_TicTacToeGame.py
+++ P1_TicTacToeGame.py
@@ -1,115 +1,80 @@
-board = {
-    'T1': '','T2': '','T3':'',
-    'M1': '','M2': '','M3':'',
-    'D1': '','D2': '','D3':''
-}
-Player = 1
-TotalMoves = 0
-EndCheck = 0
+# Tic-Tac Toe
 
-def check():
-    #Horizontal Check
-    if board['T1'] == 'X' and board['T2'] == 'X' and board['T3'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    if board['M1'] == 'X' and board['M2'] == 'X' and board['M3'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    if board['D1'] == 'X' and board['D2'] == 'X' and board['D3'] == 'X':
-        print("Player 1 Won!")
-        return 1
+def print_board(board):
+    """Print the Tic Tac Toe board."""
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
 
-    #Vertical Check
-    if board['T1'] == 'X' and board['M1'] == 'X' and board['D1'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    if board['T2'] == 'X' and board['M2'] == 'X' and board['D2'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    if board['T3'] == 'X' and board['M3'] == 'X' and board['D3'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    
-    #Diagonal Check
-    if board['T1'] == 'X' and board['M2'] == 'X' and board['D3'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    if board['T3'] == 'X' and board['D2'] == 'X' and board['M1'] == 'X':
-        print("Player 1 Won!")
-        return 1
-    
+def check_winner(board, player):
+    """Check if the player has won the game."""
+    # Check rows and columns
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)):
+            return True
+        if all(board[j][i] == player for j in range(3)):
+            return True
 
-#Player 2 Check
-#Horizontal Check
-    if board['T1'] == 'O' and board['T2'] == 'O' and board['T3'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    if board['M1'] == 'O' and board['M2'] == 'O' and board['M3'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    if board['D1'] == 'O' and board['D2'] == 'O' and board['D3'] == 'O':
-        print("Player 2 Won!")
-        return 1
+    # Check diagonals
+    if all(board[i][i] == player for i in range(3)):
+        return True
+    if all(board[i][2-i] == player for i in range(3)):
+        return True
 
-    #Vertical Check
-    if board['T1'] == 'O' and board['M1'] == 'O' and board['D1'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    if board['T2'] == 'O' and board['D2'] == 'O' and board['M2'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    if board['T3'] == 'O' and board['M3'] == 'O' and board['D3'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    
-    #Diagonal Check
-    if board['T1'] == 'O' and board['M2'] == 'O' and board['D3'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    if board['T3'] == 'O' and board['D2'] == 'O' and board['M1'] == 'O':
-        print("Player 2 Won!")
-        return 1
-    return 0
+    return False
 
-print('T1 | T2 | T3 \n')
-print('-+-+- \n')
-print('T1 | T2 | T3 \n')
-print('-+-+- \n')
-print('T1 | T2 | T3 \n')
+def is_board_full(board):
+    """Check if the board is fully occupied."""
+    for row in board:
+        if " " in row:
+            return False
+    return True
 
-while True:
-    print(board['T1']+ '|' +board['T2']+ '|' +board['T3'])
-    print('-+-+- \n')
-    print(board['M1']+ '|' +board['M2']+ '|' +board['M3'])
-    print('-+-+- \n')
-    print(board['D1']+ '|' +board['D2']+ '|' +board['D3'])
+def play_tic_tac_toe():
+    """Play Tic Tac Toe game."""
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    current_player = 0
 
-    EndCheck = check()
-    if TotalMoves == 9 or EndCheck == 1:
-        break
     while True:
-        if Player == 1:
-            p1_input = input("Enter your value: ")
-            print(p1_input)
-            #p1_input =  input('Player 1')
-            if p1_input.upper() in board and board[p1_input.upper()] == '':
-                board[p1_input.upper()] = 'X'
-                player = 2
-                break
-            else:
-                print("Wrong Input. Try Again")
-                continue
-        
-        if Player == 2:
-            p2_input =  input('Player 2 Chance')
-            if p2_input.upper() in board and board[p2_input.upper()] == '':
-                board[p2_input.upper()] = 'O'
-                player = 1
-                break
-            else:
-                print("Wrong Input. Try Again")
-                continue
+        print_board(board)
 
-                TotalMoves += 1
-            print('*************************')
-            print()
+        player_symbol = players[current_player]
+        print(f"Player {player_symbol}'s turn")
+
+        # Get player move
+        while True:
+            try:
+                block = int(input("Enter block number (1-9): "))
+                if 1 <= block <= 9:
+                    row = (block - 1) // 3
+                    col = (block - 1) % 3
+                    if board[row][col] == " ":
+                        break
+                    else:
+                        print("Block is already occupied. Try again.")
+                else:
+                    print("Invalid block number. Please enter a number between 1 and 9.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+        # Make the move
+        board[row][col] = player_symbol
+
+        # Check for win
+        if check_winner(board, player_symbol):
+            print_board(board)
+            print(f"Player {player_symbol} wins!")
+            break
+
+        # Check for tie
+        if is_board_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        # Switch player
+        current_player = (current_player + 1) % 2
+
+if __name__ == "__main__":
+    play_tic_tac_toe()
